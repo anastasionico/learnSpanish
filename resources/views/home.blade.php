1cfbd0a4a4e7c386d6a83637f0440e2460d10dff
anastasionico <nico@anastasionico.uk>
2019-02-17 10:34:41 +0000

    font added to website

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -19,7 +19,7 @@
 @endsection
 
 @section('contentOne')
-    <div class="row  p-5 text-center bg-white c-greenDark description">
+    <div class="row  p-5 text-center bg-white c-greenLight description">
         <div class="col-sm-12 col-lg-1"></div>
         <div class="col-sm-12 col-lg-10">
             We’ve brought together over ???? different Spanish verb forms all in one place. If you simply want to practice Spanish verbs, then <b>espapaya.com</b> is for you.
@@ -32,7 +32,7 @@
     <div class="row p-5 text-left bg-grayLight">
         <div class="col-sm-12 col-lg-8">
             <h2 class="c-orange">Verb Practice Gameplay</h2>
-            <p>Our verb practice gamelplay brings together the most important Spanish verb conjugations over all tenses. present, preterite, imperfect, conditional,future,present subjunctive, imperfect subjunctive, affirmative and negative iperatives.</p>
+            <p>Our verb practice gameplay brings together the most important Spanish verb conjugations over all tenses: present, preterite, imperfect, conditional, future, present subjunctive, imperfect subjunctives, affirmative and negative imperatives.</p>
         </div>
         <div class="col-sm-12 col-lg-4 text-center">
             <img src="{{asset('img/home/target.png')}}" class="w-50">
@@ -47,11 +47,10 @@
         </div>
         <div class="col-sm-12 col-lg-8">
             <h2 class="c-warning">Efficient Learning</h2>
-            <p>
-                Make your learning more efficient by creating an account. account holders can select any combination of tenses to leanr, and our bots will track your learning from one session to the next.
+            <p>Make your learning more efficient by creating an account; account holders can select any combination of tenses to learn, and our bots will track your learning from one session to the next.
             </p>
             <p>
-                <b>Click <a href="#">here</a> to create an account</b> - we won't use your details for our marketing unless you opt in and you can completely delete your personal details at any time.
+                <b>Click <a href="#">here</a> to create an account</b>we won’t use your details for our marketing unless you opt in and you can completely delete your personal details at any time.
             </p>
         </div>
     </div>
@@ -60,10 +59,7 @@
     <div class="row p-5 text-left bg-grayLight">
         <div class="col-sm-12 col-lg-8">
             <h2 class="c-info">Durable knowledge retentions</h2>
-            <p>
-                Our bots use spaced repetition, revisiting information at increasing intervals of time to produce long-term durable retention of knowledge.
-                Our advanced system also group together identica and similar verb form. so that you don't waste time learning the same verb forms.
-            </p>
+            <p>Our bots use spaced repetition, revisiting information at increasing intervals of time to produce long-term durable retention of knowledge. Our advanced systems also group together identical and similar verb forms, so that you don’t waste time learning the same verb forms.</p>
         </div>
         <div class="col-sm-12 col-lg-4 text-center">
             <img src="{{asset('img/home/brain.png')}}" class="w-50">
@@ -78,9 +74,7 @@
         </div>
         <div class="col-sm-12 col-lg-8">
             <h2 class="c-alert">Ad-free learning</h2>
-            <p>
-                create a paid account for ad-free learning and access to our entire database of verb forms.
-                <b>Click <a href="#">here</a> to create a paid account</b>
+            <p>Create a paid account for ad-free learning and access to our entire database of verb forms. <b>Click <a href="#">here</a> to create a paid account</b>
             </p>
         </div>
     </div>
---
+++ resources/views/terms.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.master')
+
+@section('contentOne')
+    <div class="row  p-5 text-left bg-greyLight c-greenLight">
+        <div class="col-sm-12 col-lg-2"></div>
+        <div class="col-sm-12 col-lg-8">
+            We’ve brought together over ???? different Spanish verb forms all in one place. If you simply want to practice Spanish verbs, then <b>espapaya.com</b> is for you.
+        </div>
+        <div class="col-sm-12 col-lg-2"></div>
+    </div>
+@endsection
+
