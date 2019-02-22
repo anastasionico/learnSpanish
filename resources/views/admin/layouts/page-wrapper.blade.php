7d45cb8c02e061da0002c34ca8ae33ffa8095bb3
anastasionico <nico@anastasionico.uk>
2019-02-22 07:50:48 +0000

    admin layout completed

--- resources/views/admin/layouts/page-wrapper.blade.php
+++ resources/views/admin/layouts/page-wrapper.blade.php
@@ -681,9 +681,9 @@
             <!-- ============================================================== -->
             <!-- footer -->
             <!-- ============================================================== -->
-            <footer class="footer text-center">
+            {{-- <footer class="footer text-center">
                All Rights Reserved by Matrix-admin. Designed and Developed by <a href="https://wrappixel.com">WrapPixel</a>.
-            </footer>
+            </footer> --}}
             <!-- ============================================================== -->
             <!-- End footer -->
             <!-- ============================================================== -->
