3d9e31a0dd3fe287f32543ef3cd84da9a365fc7c
anastasionico <nico@anastasionico.uk>
2019-02-25 06:52:46 +0000

    add disabled field to tenses

--- resources/views/start-quiz.blade.php
+++ resources/views/start-quiz.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.master')
 
 @section('contentOne')
-    <div class="row p-5 text-left bg-grayLight c-greenDark">
+    <div class="row p-5 text-left bg-grayLight c-greenDark tenses">
         <div class="col-sm-12 col-lg-2"></div>
         <div class="col-sm-12 col-lg-8 bg-white rounded py-4">
             <div class="row py-1">
@@ -31,7 +31,7 @@
                     Past Participle
                 </div>
                 <div class="col-4">
-                    <label class="switch">
+                    <label class="switch" >
                       <input type="checkbox">
                       <span class="slider round"></span>
                     </label>
@@ -43,7 +43,7 @@
                 </div>
                 <div class="col-4">
                     <label class="switch">
-                      <input type="checkbox">
+                      <input type="checkbox" >
                       <span class="slider round"></span>
                     </label>
                 </div>        
@@ -108,6 +108,125 @@
         <div class="col-sm-12 p-5 text-center bg-grayLight">
             <a href='/quiz' class="button bg-orange c-orange">
                 Start
+            </a>
+        </div>
+    </div>
+
+    <div class="row p-5 text-left bg-grayLight c-greenDark tenses">
+        <div class="col-sm-12 col-lg-2"></div>
+        <div class="col-sm-12 col-lg-8 bg-white rounded py-4">
+            <div class="row py-1">
+                <div class="col-8 pl-5">
+                    Present
+                </div>
+                <div class="col-4">
+                    <label class="switch">
+                      <input type="checkbox">
+                      <span class="slider round"></span>
+                    </label>
+                </div>        
+            </div>
+            <div class="row py-1">
+                <div class="col-8 pl-5">
+                    Preterite
+                </div>
+                <div class="col-4">
+                    <label class="switch">
+                      <input type="checkbox">
+                      <span class="slider round"></span>
+                    </label>
+                </div>        
+            </div>
+            <div class="row py-1">
+                <div class="col-8 pl-5">
+                    Past Participle
+                </div>
+                <div class="col-4">
+                    <label class="switch" >
+                      <input type="checkbox">
+                      <span class="slider round"></span>
+                    </label>
+                </div>        
+            </div>
+            <div class="row py-2">
+                <div class="col-12 text-center"> 
+                    <a href='/quiz' class="button bg-orange c-orange">
+                        <small>Register to unlock the other tenses</small> 
+                    </a>
+                    
+                </div>        
+            </div>
+            <div class="row py-1">
+                <div class="col-8 pl-5 disabled">
+                    Imperfect
+                </div>
+                <div class="col-4">
+                    <label class="switch">
+                      <input type="checkbox"  disabled>
+                      <span class="slider round"></span>
+                    </label>
+                </div>        
+            </div>
+            <div class="row py-1">
+                <div class="col-8 pl-5 disabled">
+                    Subjuntive
+                </div>
+                <div class="col-4">
+                    <label class="switch">
+                      <input type="checkbox" disabled>
+                      <span class="slider round"></span>
+                    </label>
+                </div>        
+            </div>
+            <div class="row py-1">
+                <div class="col-8 pl-5 disabled">
+                    Conditional
+                </div>
+                <div class="col-4">
+                    <label class="switch">
+                      <input type="checkbox" disabled>
+                      <span class="slider round"></span>
+                    </label>
+                </div>        
+            </div>
+            <div class="row py-1">
+                <div class="col-8 pl-5 disabled">
+                    Future
+                </div>
+                <div class="col-4">
+                    <label class="switch">
+                      <input type="checkbox" disabled>
+                      <span class="slider round"></span>
+                    </label>
+                </div>        
+            </div>
+            <div class="row py-1">
+                <div class="col-8 pl-5 disabled">
+                    Imperfect
+                </div>
+                <div class="col-4">
+                    <label class="switch">
+                      <input type="checkbox" disabled>
+                      <span class="slider round"></span>
+                    </label>
+                </div>        
+            </div>
+            <div class="row py-1">
+                <div class="col-8 pl-5 disabled">
+                    Imperative
+                </div>
+                <div class="col-4">
+                    <label class="switch">
+                      <input type="checkbox" disabled>
+                      <span class="slider round"></span>
+                    </label>
+                </div>        
+            </div>
+        </div>
+        <div class="col-sm-12 col-lg-2"></div>
+        <div class="col-sm-12 p-5 text-center bg-grayLight">
+            <a href='/quiz' class="button bg-orange c-orange">
+                Register
             </a>
         </div>
     </div>
