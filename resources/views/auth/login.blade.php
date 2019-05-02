30d8afe5957ae2e5f5c9931618073cbeb395fdf0
anastasionico <nico@anastasionico.uk>
2019-05-02 05:56:59 +0100

    style added to login page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,8 +1,8 @@
-@extends('layouts.app')
+@extends('layouts.master')
 
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
+
+@section('contentOne')
+    <div class="row justify-content-center loginForm mb-5">
         <div class="col-md-8">
             <div class="card">
                 <div class="card-header">{{ __('Login') }}</div>
@@ -53,12 +53,12 @@
 
                         <div class="form-group row mb-0">
                             <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
+                                <button type="submit" class="button bg-greenLight c-white border-white">
                                     {{ __('Login') }}
                                 </button>
 
                                 @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
+                                    <a class="" href="{{ route('password.request') }}">
                                         {{ __('Forgot Your Password?') }}
                                     </a>
                                 @endif
@@ -69,5 +69,4 @@
             </div>
         </div>
     </div>
-</div>
 @endsection
