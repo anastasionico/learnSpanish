8abbafdf98647aebfe3f1dad3bcbd6f1c5f57083
anastasionico <nico@anastasionico.uk>
2019-05-13 06:23:06 +0100

    edited navbar-half

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -11,47 +11,49 @@
             </button>
 
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                
                 <!-- Left Side Of Navbar -->
-                <ul class="navbar-nav mr-auto">
-
-                </ul>
+                {{-- <ul class="navbar-nav mr-auto"></ul> --}}
 
                 <!-- Right Side Of Navbar -->
-                <ul class="navbar-nav ml-auto">
-                    <!-- Authentication Links -->
-                    @guest
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{ route('login') }}">
-                                {{ __('Login') }}
-                            </a>
-                        </li>
-                        @if (Route::has('register'))
+                <div class="navbar-half ml-auto">
+                    <ul class="navbar-nav">
+                        <!-- Authentication Links -->
+                        @guest
                             <li class="nav-item">
-                                <a class="nav-link filled" href="{{ route('register') }}">{{ __('Sign Up') }}</a>
+                                <a class="nav-link" href="{{ route('login') }}">
+                                    {{ __('Login') }}
+                                </a>
                             </li>
-                        @endif
-                    @else
-                        <li class="nav-item dropdown">
-                            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                {{ Auth::user()->name }}
-                                <span class="caret"></span>
-                            </a>
-
-                            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                <a class="dropdown-item" 
-                                    href="{{ route('logout') }}"
-                                    onclick="event.preventDefault();
-                                    document.getElementById('logout-form').submit();">
-                                    {{ __('LOGOUT') }}
+                            @if (Route::has('register'))
+                                <li class="nav-item">
+                                    <a class="nav-link filled" href="{{ route('register') }}">{{ __('Sign Up') }}</a>
+                                </li>
+                            @endif
+                        @else
+                            <li class="nav-item dropdown">
+                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                    {{ Auth::user()->name }}
+                                    <span class="caret"></span>
                                 </a>
 
-                                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                    @csrf
-                                </form>
-                            </div>
-                        </li>
-                    @endguest
-                </ul>
+                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item" 
+                                        href="{{ route('logout') }}"
+                                        onclick="event.preventDefault();
+                                        document.getElementById('logout-form').submit();">
+                                        {{ __('LOGOUT') }}
+                                    </a>
+
+                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                        @csrf
+                                    </form>
+                                </div>
+                            </li>
+                        @endguest
+                    </ul>    
+                </div>
+                
             </div>
         </div>
     </nav>
