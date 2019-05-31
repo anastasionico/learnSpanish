2d71efc1c3b139d8cca8c738fec0c123af30ba08
anastasionico <nico@anastasionico.uk>
2019-05-31 23:20:26 +0100

    added field to users

---
+++ database/migrations/2019_05_31_221214_add_field_to_users.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddFieldToUsers extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->date('dob')->nullable();
+            $table->string('sex');
+            $table->string('country');
+            $table->string('first_language');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('dob');
+            $table->dropColumn('sex');
+            $table->dropColumn('country');
+            $table->dropColumn('first_language');
+        });
+    }
+}
