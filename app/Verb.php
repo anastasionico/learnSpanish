e01c13cf70c2f7a719fef422f1bac9e27e3b463c
anastasionico <nico@anastasionico.uk>
2019-06-06 06:13:55 +0100

    fixed tenseController create

--- app/Verb.php
+++ app/Verb.php
@@ -17,13 +17,20 @@ class Verb extends Model
 
 	public function addTense($request)
 	{
-		return Tense::create(
-            $request->validate([
-                'verb_id' => ['required'],
-                'sequence_number' => ['required','numeric'],
-                'name' => ['required','string'],
-                'is_free' => [],
-            ])
-        );
+        $is_free = (request()->has('is_free'))? 1 : 0;
+
+		$request->validate([
+            'verb_id' => ['required'],
+            'sequence_number' => ['required','numeric'],
+            'name' => ['required','string'],
+            'is_free' => [],
+        ]);
+        
+        return Tense::create([
+            'verb_id' => request('verb_id'),
+            'sequence_number' => request('sequence_number'),
+            'name' => request('name'),
+            'is_free' => $is_free,
+        ]);
 	}
 }
