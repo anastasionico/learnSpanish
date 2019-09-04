a6c3141d89342cc6e8fc00c72d288f01ad667205
anastasionico <nico@anastasionico.uk>
2019-09-04 22:18:34 +0100

    add send Ian email button

--- resources/views/admin/messages/index.blade.php
+++ resources/views/admin/messages/index.blade.php
@@ -13,7 +13,6 @@
                 <table class="table">
                   <thead>
                     <tr>
-                      <th scope="col">#</th>
                       <th scope="col">Name</th>
                       <th scope="col">Email</th>
                       <th scope="col">Message</th>
@@ -22,19 +21,23 @@
                   <tbody>
                     @foreach($messages as $message) 
                       <tr>
-                        <th scope="row">{{$message->id}}</th>
                         <td>{{$message->name}}</td>
                         <td>{{$message->email}}</td>
                         <td style="max-width: 380px">{{$message->message}}</td>
                         <td>
-                          <a href="mailto:{{$message->email}}?Subject=From Espapaya" target="_top" class="btn btn-info btn-sm d-inline">
-                                <i class="fas fa-envelope"></i>
+                          <a href="mailto:{{$message->email}}?subject=From Espapaya" target="_top" class="btn btn-info btn-sm d-block">
+                            <i class="fas fa-share"> Answer {{ucfirst($message->name)}}</i>
                           </a>
+
+                          <a href="mailto:i_wall@hotmail.co.uk?subject=From Espapaya&body={{$message->message}}" class="btn btn-warning btn-sm d-block">
+                            <i class="fas fa-envelope"> Send to my Inbox</i>
+                          </a>
+
                           
-                          <form method="POST" action="messages/{{$message->id}}" class="d-inline">
+                          <form method="POST" action="messages/{{$message->id}}" class="">
                             @method('DELETE')
                             @csrf
-                            <button type="submit" class="btn btn-danger btn-sm d-inline deleteItem" style="margin:0.2rem;cursor:pointer;">
+                            <button type="submit" class="btn btn-danger btn-sm deleteItem d-block" style="margin:0.2rem;cursor:pointer;">
                               <i class="fas fa-trash"></i>
                             </button>    
                           </form>
